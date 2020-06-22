c3ae0e38b247aa4bbc7586bf0895a021887009ad
Vivien Galuchot <vivien.galuchot@gmail.com>
2020-06-22 11:23:31 +0200

    improve p2p test

--- rad/p2p.js
+++ rad/p2p.js
@@ -1,7 +1,3 @@
-/**
- * inspired from https://stackoverflow.com/questions/54980799/webrtc-datachannel-with-manual-signaling-example-please
- */
-
 class P2PConnection {
     constructor() {
         const config = { iceServers: [{ urls: "stun:stun.1.google.com:19302" }] };
@@ -9,161 +5,89 @@ class P2PConnection {
 
         this.isInitiator = null;
 
+        this.onStateChange = (state) => { };
+
         this.pc.oniceconnectionstatechange = (evt) => {
             console.log("ice state change : ", this.pc.iceConnectionState);
+            this.onStateChange(this.getState());
         };
         this.pc.onsignalingstatechange = (evt) => {
             console.log("signaling state change : ", this.pc.signalingState);
+            this.onStateChange(this.getState());
         };
+        this.pc.addEventListener("negotiationneeded", event => {
+            console.log("negotiation needed");
+        });
+
+        this.tagCompress = tag => LZString.compressToBase64(tag);
+        this.tagDecompress = tag => LZString.decompressFromBase64(tag);
+    }
+
+    // monitoring
+
+    getState() {
+        return `${this.pc.iceConnectionState} ${this.pc.signalingState}`;
     }
 
     // initiator
 
+    /**
+     * Initiate a connection.
+     * - The returned string called "offer" shall be sent to the other peer via any channel.
+     */
     async createOffer() {
         if (this.isInitiator != null)
-            throw new Error("already initiated");
+            console.error("already initiated");
         this.isInitiator = true;
 
-        await this.pc.setLocalDescription(await this.pc.createOffer());
+        await this.pc.setLocalDescription();
         return new Promise(resolve => {
             this.pc.onicecandidate = ({ candidate }) => {
                 if (candidate != null)
                     return;
-                resolve(btoa(this.pc.localDescription.sdp));
+                resolve(this.tagCompress(this.pc.localDescription.sdp));
             }
         });
     }
 
+    /**
+     * Complete the connection by consuming the peer answer.
+     * - Once the peer has the offer, it must send back an "answer" to complete the connection.
+     */
     async consumeAnswer(peerAnswer) {
         if (this.isInitiator != true)
-            throw new Error("not initiator");
+            console.error("not initiator");
 
         if (this.pc.signalingState != "have-local-offer")
-            throw new Error("unexpected signaling state");
+            console.error("unexpected signaling state");
 
-        console.log("answer is", atob(peerAnswer));
-        this.pc.setRemoteDescription({ type: "answer", sdp: atob(peerAnswer) });
+        this.pc.setRemoteDescription({ type: "answer", sdp: this.tagDecompress(peerAnswer) });
     }
 
     // non initiator
 
+    /**
+     * Non initiator part. Consume an offer sent from the connection initiator and return an answer.
+     * The answer shall be sent back to the initiator via any channel.
+     */
     async consumeOfferAndGetAnswer(peerOffer) {
         if (this.isInitiator != null)
-            throw new Error("already initiated");
+            console.error("already initiated");
         this.isInitiator = false;
 
         if (this.pc.signalingState != "stable")
-            throw new Error("unexpected signaling state");
+            console.error("unexpected signaling state");
 
+        await this.pc.setRemoteDescription({ type: "offer", sdp: this.tagDecompress(peerOffer) });
 
-        console.log("offer is", atob(peerOffer));
-        await this.pc.setRemoteDescription({ type: "offer", sdp: atob(peerOffer) });
-
-        await this.pc.setLocalDescription(await this.pc.createAnswer());
+        // await this.pc.setLocalDescription(await this.pc.createAnswer());
+        await this.pc.setLocalDescription();
         return new Promise(resolve => {
             this.pc.onicecandidate = ({ candidate }) => {
                 if (candidate != null)
                     return;
-                resolve(btoa(this.pc.localDescription.sdp));
+                resolve(this.tagCompress(this.pc.localDescription.sdp));
             }
         });
     }
-}
-
-
-let connection;
-
-document.addEventListener("DOMContentLoaded", evt => {
-    connection = new P2PConnection();
-
-    const dc = connection.pc.createDataChannel("chat", { negotiated: true, id: 0 });
-    dc.onopen = () => {
-        for (let el of document.querySelectorAll(".initiator")) {
-            el.classList.add("js-hidden");
-        }
-        for (let el of document.querySelectorAll(".non-initiator")) {
-            el.classList.add("js-hidden");
-        }
-        for (let el of document.querySelectorAll(".non-initiator-2")) {
-            el.classList.add("js-hidden");
-        }
-        for (let el of document.querySelectorAll(".success")) {
-            el.classList.remove("js-hidden");
-        }
-    };
-
-    log = (msg) => {
-        chatLog.innerHTML += `${msg}<br>`;
-    }
-    chatInput.onkeypress = function (e) {
-        if (e.keyCode != 13)
-            return;
-        try {
-            dc.send(chatInput.value);
-        } catch (e) {
-        }
-        log(`- ${chatInput.value}`);
-        chatInput.value = "";
-    };
-
-    dc.onmessage = e => log(`> ${e.data}`);
-
-    document.querySelector("#peerTag").value = "";
-});
-
-function initiate() {
-    joinBtn.classList.add("js-hidden");
-    initiateBtn.classList.add("js-hidden");
-    for (let el of document.querySelectorAll(".initiator")) {
-        el.classList.remove("js-hidden");
-    }
-
-    connection.createOffer()
-        .then((offer) => {
-            console.log("createOffer ok");
-            document.querySelector("#offer").innerText = offer;
-        })
-        .catch(reason => {
-            console.log("createOffer error", reason);
-        });
-}
-
-function terminate() {
-    let peerAnswer = document.querySelector("#peerTag").value;
-    connection.consumeAnswer(peerAnswer)
-        .then(() => {
-            console.log("consumeAnswer ok");
-        })
-        .catch(reason => {
-            console.log("consumeAnswer error", reason);
-        });
-}
-
-function join() {
-    joinBtn.classList.add("js-hidden");
-    initiateBtn.classList.add("js-hidden");
-    for (let el of document.querySelectorAll(".non-initiator")) {
-        el.classList.remove("js-hidden");
-    }
-}
-
-function reach() {
-    for (let el of document.querySelectorAll(".non-initiator-2")) {
-        el.classList.remove("js-hidden");
-    }
-
-    let peerOffer = document.querySelector("#peerTag").value;
-    connection.consumeOfferAndGetAnswer(peerOffer)
-        .then((answer) => {
-            console.log("consumeOfferAndGetAnswer ok");
-            document.querySelector("#answer").innerText = answer;
-        })
-        .catch(reason => {
-            console.log("consumeOfferAndGetAnswer error", reason);
-        });
-}
-
-function send(text) {
-    console.log(text);
-    connection.dc.send(text);
 }
